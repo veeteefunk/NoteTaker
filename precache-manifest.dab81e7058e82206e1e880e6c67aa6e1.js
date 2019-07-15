620e5c8b5e9a2d88ab286419536670f691a5d74c
veeteefunk <victormarinviloria@gmail.com>
2019-07-15 23:34:43 +0200

    Updates

---
+++ precache-manifest.dab81e7058e82206e1e880e6c67aa6e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347db5cd3046ce0adae3d9f02ce28c75",
+    "url": "/NoteTaker/index.html"
+  },
+  {
+    "revision": "ad59773d9ade1de974ba",
+    "url": "/NoteTaker/static/css/main.8f958165.chunk.css"
+  },
+  {
+    "revision": "3a3bad03c2205b39645f",
+    "url": "/NoteTaker/static/js/2.6b12c761.chunk.js"
+  },
+  {
+    "revision": "ad59773d9ade1de974ba",
+    "url": "/NoteTaker/static/js/main.9e712f2d.chunk.js"
+  },
+  {
+    "revision": "1657df71eec30e6145b5",
+    "url": "/NoteTaker/static/js/runtime~main.877c6e19.js"
+  },
+  {
+    "revision": "bbac3e7db2d6a130acde66f679946a69",
+    "url": "/NoteTaker/static/media/landscape.bbac3e7d.jpg"
+  }
+]);
